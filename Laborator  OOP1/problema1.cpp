b32e3b9cc1fe4cac57b1c0ce0ae22f687c4ba877
punctIT <teodorivan2005@gmail.com>
2025-02-25 21:37:28 +0200

    rezolbare problema 1

--- Laborator  OOP1/problema1.cpp
+++ Laborator  OOP1/problema1.cpp
@@ -7,26 +7,26 @@ using namespace std;
 int main()
 {
 	FILE* fisier;
+	int suma = 0;
 	if (fopen_s(&fisier, "in.txt", "r") != 0) {
 		printf("eroare");
 	}
-	
 	else {
 
 		char s[100];
 		while (fgets(s, 100, fisier))
 		{
-
-			int c = 0, suma = 0;
-			while (s[c] != 0)
-			{	
-				suma += s[c];
-				cout << s << endl;
-				++c;
+			int numar = 0;
+			int i = 0;
+			while (s[i] != 0)
+			{
+				if(s[i]>47&&s[i]<='9')
+					numar = numar * 10 + s[i] - 48;
+				i++;
 			}
-			suma -= (c * 48);
-			cout << suma << " ";
+			suma += numar;
 		}
 	}
+	printf("%d ", suma);
 
 }
