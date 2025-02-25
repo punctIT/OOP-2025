4432d49004db14bef1ad280bd769b533ef27824e
punctIT <teodorivan2005@gmail.com>
2025-02-25 19:52:06 +0200

    problema 1 deloc facuta

--- Laborator  OOP1/problema1.cpp
+++ Laborator  OOP1/problema1.cpp
@@ -4,20 +4,29 @@
 using namespace std;
 
 
-
 int main()
 {
 	FILE* fisier;
-	if (fopen_s(&fisier, "in.txt", "r") != 0){
+	if (fopen_s(&fisier, "in.txt", "r") != 0) {
 		printf("eroare");
 	}
+	
 	else {
-		
+
 		char s[100];
-		while (fgets(s, 200, fisier))
+		while (fgets(s, 100, fisier))
 		{
-			
+
+			int c = 0, suma = 0;
+			while (s[c] != 0)
+			{	
+				suma += s[c];
+				cout << s << endl;
+				++c;
+			}
+			suma -= (c * 48);
+			cout << suma << " ";
 		}
 	}
-	
+
 }
